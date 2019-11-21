3bb66e0961b78d4cceea81e2699189685e19d0bf
Amelia He <amelia.he98@gmail.com>
2019-11-21 01:13:00 -0800

    updated database

--- Database/carrental.sql
+++ Database/carrental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 21, 2019 at 09:56 AM
+-- Generation Time: Nov 21, 2019 at 09:57 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.32
 
