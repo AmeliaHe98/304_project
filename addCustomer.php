c5bdba8bd0ca71d0a8f65e493ae48489dd05654d
YUXIN-Z <zhangyuxin201709@gmail.com>
2019-11-21 13:21:31 -0800

    modified ui + reservation

--- addCustomer.php
+++ addCustomer.php
@@ -1,6 +1,6 @@
 <?php
 // ? you can comment it out to try
-require_once("database.php");
+// require_once("database.php");
 if (isset($_POST["submit"])){
     if (empty($_POST["cellphone_number"])
     || empty($_POST["name"])
@@ -34,36 +34,97 @@ if (isset($_POST["submit"])){
 
 
 <html>
+
 <head>
-<title>Add Customer</title>
-</head>
-<body>
-<form action = "http://localhost:8080/304_project/addCustomer.php" method = "post">
+	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz|Open+Sans:400,600,700|Oswald|Electrolize' rel='stylesheet' type='text/css' />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+	<title>Car Rental | Home</title>
+	
+	<link rel="shortcut" href="images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" media="screen" />
+	<link rel="stylesheet" href="css/skeleton.css" media="screen" />
+	<link rel="stylesheet" href="sliders/flexslider/flexslider.css" media="screen" />
+	<link rel="stylesheet" href="fancybox/jquery.fancybox.css" media="screen" />
 
- <b> Add a New Customer</b>
+	<!-- HTML5 Shiv + detect touch events -->
+	<script type="text/javascript" src="js/modernizr.custom.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<body class="menu-1 h-style-1 text-1">
 
- <p> Cellphone Number:
- <input type = "text" name = "cellphone_number" size = 30 value = "">
- </p>
+<div class="wrap">
 
- <p> Name:
- <input type = "text" name = "name" size = 30 value = "">
- </p>
+<header id="header" class="clearfix">
+		
+        <a href="index.html" id="logo"><img src="images/logo.png" alt="Car Rental" /></a>
+        
+	</header><!--/ #header-->
 
- <p> Address:
- <input type = "text" name = "address" size = 30 value = "">
- </p>
-
- <p> Driver license:
- <input type = "text" name = "dlicense" size = 30 value = "">
- </p>
-
-
+    
+ <div style="text-align:center">
 <p>
-<input type = "submit" name = "submit" value = "Add" />
-</p>
 
+<body>
+<p>
+<section class="container py-2 mb-4">
+      <div class="row">
+        <div class="offset-sm-3 col-sm-6" style="min-height:400px;">
+
+          <div class="card bg-secondary text-light">
+            <div class="card-header">
+              <h1>Add a New Customer</h1>
+              <br>
+              </div>
+              <div class="card-body bg-dark">
+              <form class="" action="addCustomer.php" method="post">
+                <div class="form-group">
+                  <label for="cellphone_number"><span class="FieldInfo"><h4>Cellphone Number</h4></span></label>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text text-white bg-info"> <i class="fas fa-user"></i> </span>
+                    </div>
+                    <input type="text" class="form-control" name="cellphone_number" id="cellphone_number" size = 100 style="height:30px; width: 500px" value="">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label for="name"><span class="FieldInfo"><h4>Name</h4></span></label>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text text-white bg-info"> <i class="fas fa-lock"></i> </span>
+                    </div>
+                    <input type="text" class="form-control" name="name" size = 100 style="height:30px; width: 500px" id="name" value="">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label for="address"><span class="FieldInfo"><h4>Address</h4></span></label>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text text-white bg-info"> <i class="fas fa-lock"></i> </span>
+                    </div>
+                    <input type="text" class="form-control" name="address" size = 100 style="height:30px; width: 500px" id="address" value="">
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label for="dlicense"><span class="FieldInfo"><h4>Driver license</h4></span></label>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text text-white bg-info"> <i class="fas fa-lock"></i> </span>
+                    </div>
+                    <input type="text" class="form-control" name="dlicense" size = 100  style="height:30px; width: 500px" id="dlicense" value="">
+                  </div>
+                </div>
+                <p>
+                    <br><br><br>
+                    <input type = "submit" name = "submit" class="btn btn-info btn-block" style="height: 500px; width: 80px; left: 250; top: 250;" value = "SIGN UP" />
+                </p>
+            </p>
+        </div>
 </form>
+
 
 </body>
 <html>
