f98a2feebe9727f03768a7dd5b021b64ee56b14e
YUXIN-Z <zhangyuxin201709@gmail.com>
2019-11-23 18:11:10 -0800

    aiaia

--- dailyReturnsByBranch.php
+++ dailyReturnsByBranch.php
@@ -1,3 +1,5 @@
 <?php
 require_once("DB.php");
+global $ConnectingDB;
+$query
 ?>
--- reportGenerator.php
+++ reportGenerator.php
@@ -9,9 +9,9 @@
 		<title><h2>Select the report you want to view !</h2></title>
         </head>
 	<body>
-    <button type="button" onclick = "report/dailyRentals.php">Daily Rentals</button>
-    <button type="button " onclick = "report/dailyRentalsByBranch.php">Daily Rentals per Branch</button>
-    <button type="button" onclick = "report/dailyReturns.php">Daily Returns</button>
-    <button type="button" onclick = "report/dailyReturnsByBranch.php">Daily Returns per Branch</button>
+    <a href="report/dailyRentals.php"><button type="button" onclick = "report/dailyRentals.php">Daily Rentals</button></a>
+    <a href="report/dailyRentals.php"><button type="button " onclick = "report/dailyRentalsByBranch.php">Daily Rentals per Branch</button></a>
+    <a href="report/dailyRentals.php"><button type="button" onclick = "report/dailyReturns.php">Daily Returns</button></a>
+    <a href="report/dailyRentals.php"><button type="button" onclick = "report/dailyReturnsByBranch.php">Daily Returns per Branch</button></a>
     </body>
 </html>
