b3f396937dbf10fbb2009cb1752ef834a3d73494
YUXIN-Z <zhangyuxin201709@gmail.com>
2019-11-23 18:49:35 -0800

    kuqi

--- DB.php
+++ DB.php
@@ -2,5 +2,3 @@
 $DSN='mysql:host=localhost; dbname=carrental';
 $ConnectingDB = new PDO($DSN, 'root', '');
 $conn = $ConnectingDB ;
-
-$ConnectingDB = new PDO($dbServername, $dbUsername, $dbPassword);
