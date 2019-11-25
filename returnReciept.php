8e66962261e73788e2bcdc95508d79305acafcad
YUXIN-Z <zhangyuxin201709@gmail.com>
2019-11-24 19:58:01 -0800

    hello

--- returnReciept.php
+++ returnReciept.php
@@ -3,40 +3,54 @@ require_once("DB.php");
 global $ConnectingDB;
 $Parameter = $_GET["id"];
 
-if (isset($_POST["submit"])){
-    if (empty($_POST["TIMEID"])
-    || empty($_POST["ODOMETER"])
-    || empty($_POST["DATE_ID"])
-    || empty($_POST["FULLTANK"])
-    || empty($_POST["VALUE_ID"])) {
+if (isset($_GET["submit"])){
+    if (empty($_GET["TIMEID"])
+    || empty($_GET["ODOMETER"])
+    || empty($_GET["DATE_ID"])
+    || empty($_GET["FULLTANK"])
+    || empty($_GET["VALUE_ID"])) {
         echo "ERROR: HEY! You are creating invaild information! <br/>";
     } else {
 
 // add the information into returns
 // SELECT THIS CAR THROUGH VLICENSE and then 
-$query_select = "SELECT * FROM Renatls 
-WHERE VLICENSE = $Parameter";
+$query_select = "SELECT * FROM Rentals 
+WHERE VLICENSE = :Parameter";
 $stmt_select = $ConnectingDB->prepare($query_select);
-$stmt_select->execute();
+$stmt_select->bindValue(':Parameter', $Parameter);
+$EXECUTE = $stmt_select->execute();
 $DataRows_select = $stmt_select->fetch();
+if (!$EXECUTE) {
+    echo "hello";
+    print_r($stmt_select->errorInfo());
+}
 if ($DataRows_select != FALSE) {
 $RID = $DataRows_select["RID"];
 $CONFNO= $DataRows_select["CONFNO"];
 
-$DATE_ID = $_POST["DATE_ID"];
-$TIMEID =  $_POST["TIMEID"];
-$ODOMETER = $_POST["ODOMETER"];
-$FULLTANK = $_POST["FULLTANK"];
-$VALUE_ID = $_POST["VALUE_ID"];
+$DATE_ID = $_GET["DATE_ID"];
+$TIMEID =  $_GET["TIMEID"];
+$ODOMETER = $_GET["ODOMETER"];
+$FULLTANK = $_GET["FULLTANK"];
+$VALUE_ID = $_GET["VALUE_ID"];
 $query_return = "INSERT INTO ReturnCar (RID, DATE_ID, TIME_ID, ODOMETER, FULLTANK, VALUE_ID) 
-Values ($RID, $DATE_ID, $TIMEID, $ODOMETER, $FULLTANK, $VALUE_ID)";
+Values (:RID, :DATE_ID, :TIMEID, :ODOMETER, :FULLTANK, :VALUE_ID)";
 $stmt_return = $ConnectingDB->prepare($query_return);
+$stmt_return->bindValue(':RID', $RID);
+$stmt_return->bindValue(':DATE_ID', $DATE_ID);
+$stmt_return->bindValue(':TIMEID', $TIMEID);
+$stmt_return->bindValue(':ODOMETER', $ODOMETER);
+$stmt_return->bindValue(':FULLTANK', $FULLTANK);
+$stmt_return->bindValue(':VALUE_ID', $VALUE_ID);
 $Execute = $stmt_return->execute();
 if ($Execute) {
     echo 99;
-}
 }else{
+    print_r($stmt_return->errorInfo());
     echo "error";
+}
+}
+    }
 }
 
 
@@ -48,5 +62,4 @@ echo $CONFNO ;
 echo $DATE_ID ;
 echo $TIMEID ;
 echo $VALUE_ID;
-    }
-}
+
