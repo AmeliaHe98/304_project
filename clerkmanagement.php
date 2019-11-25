1252faf7465d7cdad6ebf58792f050a8066b39c2
Amelia He <amelia.he98@gmail.com>
2019-11-24 16:41:25 -0800

    Revert "Merge branch 'master' of https://github.com/AmeliaHe98/304_project"
    This reverts commit 5a8f238ea3edba6ec341974258165ef402c0b13b, reversing
    changes made to 74c816feaa878f76821497392aed4587d865ffa0.

--- clerkmanagement.php
+++ clerkmanagement.php
@@ -53,7 +53,7 @@
 <div class="panel-body">
 <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
 <div>
-<form class="" action="clerkmanagement.php" method="get">
+<form class="" action="clerkmanagement.php" method="POST">
 <input type="text" name="Search" value="" placeholder="Search by vtname/ vlicense/ status" size = 100><br><br>
 <input type="submit" name="SearchButton" value="Show record">
 </form>
@@ -78,22 +78,20 @@
 </thead>
 
         <?php
-		if (isset($_GET["SearchButton"])) {
+		if (isset($_POST["SearchButton"])) {
         ?>
             <div>
 
             <?php
             global $ConnectingDB;
-            $Search = $_GET["Search"];
+            $Search = $_POST["Search"];
+            $query = "SELECT * FROM Vehicle";
             if ($Search != null) {
-                echo $Search;
                 $query = "SELECT * FROM Vehicle 
-                      WHERE VTNAME = :search OR VLICENSE = :search OR STATUS = :search";
-            }else{
-                $query = "SELECT * FROM Vehicle";
+                      WHERE VTNAME = '$Search' OR VLICENSE = $Search OR STATUS = '$Search'";
             }
+            
             $stmt = $ConnectingDB->prepare($query);
-            $stmt->bindValue(':search', $Search);
             $stmt->execute();
             while ($DataRows = $stmt->fetch()) {
                 $VID = $DataRows["VID"];
@@ -102,12 +100,11 @@
                 $MODEL = $DataRows["MODEL"];
                 $YEAR  = $DataRows["YEAR"];
                 $COLOR = $DataRows["COLOR"];
-                $ODOMETER = $DataRows["ODOMETER"];
-                $STATUS = $DataRows["STATUS"];
-                $VTNAME = $DataRows["VTNAME"];
-                $LOCATION = $DataRows["LOCATION_ID"];
-                $CITY  = $DataRows["CITY"];
-
+                 $ODOMETER = $DataRows["ODOMETER"];
+                 $STATUS = $DataRows["STATUS"];
+                 $VTNAME = $DataRows["VTNAME"];
+                 $LOCATION = $DataRows["LOCATION_ID"];
+                 $CITY  = $DataRows["CITY"];
                  ?>
             <tr>
             <td><?php echo $VID; ?></td>
